715c458c941de2c044a1b3e5ded596423d3ad25c
Bryce Howell <brycehwl3@yahoo.com>
2017-05-12 13:28:54 -0400

    Include NeoPixel library
    Setup of the NeoPixel strip to communicate with Arduino

--- Scripts/NeoPixel.ino
+++ Scripts/NeoPixel.ino
@@ -1,7 +1,13 @@
 //Neopixel main script
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
